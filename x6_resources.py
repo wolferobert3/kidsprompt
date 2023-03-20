77c82642198387d1e8fa33408ddc46e60fc97b48
Robert_Wolfe <wolferobert3@gmail.com>
2023-03-19 19:53:24 -0700

    Add new admission images for x6

--- x6_resources.py
+++ x6_resources.py
@@ -8,20 +8,18 @@ LOGFILE = 'logs/test_log.txt'
 
 # Define an image classifier
 model = ImageClassifier()
-model.init_logfile(LOGFILE)
-
-# Define an image classifier
-model = ImageClassifier()
 
 # Set model parameters
 model.init_logfile(LOGFILE)
 
 TEXT_CLASSES = ['monster', 'not a monster', 'maybe a monster']
+TEXT_CLASSES = ['dog', 'not a dog']
 IMAGE_DICT = {'monster': 'admission_images/green_check.png', 'not a monster': 'admission_images/stop_sign.png', 'maybe a monster': 'admission_images/question_mark.png'}
+IMAGE_DICT = {'dog': 'admission_images/door_open.jpg', 'not a dog': 'admission_images/door_close.jpg'}#, 'maybe a monster': 'admission_images/question_mark.png'}
 
 def monster_wrapper(image):
 
-    dict_probs = model.predict(image, TEXT_CLASSES)
+    dict_probs, dict_logs = model.predict(image, TEXT_CLASSES)
     top_class = max(dict_probs, key=dict_probs.get)
     
     return dict_probs, IMAGE_DICT[top_class]
@@ -39,8 +37,8 @@ with gr.Blocks() as demo:
                 image_1 = gr.Image(source='webcam', streaming=True)
                 submission_button = gr.Button(value="Check Admission")
 
-            outputs = gr.Label(num_top_classes=3, label="Output", name="output")
-            output_image = gr.Image(label="Output", value='admission_images/question_mark.png')
+            outputs = gr.Label(num_top_classes=3, label="Output")
+            output_image = gr.Image(label="Output", value='admission_images/background_starting.jpg')
 
         submission_button.click(monster_wrapper, image_1, [outputs, output_image])
         
